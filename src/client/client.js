8d7b487905b01cf33975ff29d6d2f39bde7909f2
Brandon Morrissey <bmorrissey95@gmail.com>
2020-05-26 21:17:25 +1000

    Basic 3js demo

--- src/client/client.js
+++ src/client/client.js
@@ -1,1 +1,38 @@
-console.log('Hello...');
+import * as THREE from 'three';
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 10;
+
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setClearColor(0x000000, 1);
+
+document.body.appendChild(renderer.domElement);
+
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial({ color: 0x87E0FF });
+const cube = new THREE.Mesh(geometry, material);
+
+console.log(cube);
+
+scene.add(cube);
+
+function animate() {
+	requestAnimationFrame(animate);
+	renderer.render(scene, camera);
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+}
+
+animate();
+
+function onWindowResize() {
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+}
+
+window.addEventListener('resize', onWindowResize);
